ff35bb2ed9fc2d9c10abfbc3532f13aaae154b00
BASTARDIE Jérémy <BASTARDIE Jrmy>
2016-11-20 22:18:23 +0100

    menu opérationnel au clic, changement de scene ok

---
+++ Assets/Scripts/onClickMenuSelect.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class onClickMenuSelect : MonoBehaviour {
+
+    public GameObject snowboarder;
+    Animator animationObj;
+
+    // Use this for initialization
+    void Start () {
+        animationObj = snowboarder.GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                switch (hit.collider.name)
+                {
+                    case "Play":
+                        Debug.Log("Play Game!");
+                        animationObj.CrossFade("pointing",0.05f);
+                        StartCoroutine(MyCoroutine());
+
+                        //snowboarder.transform.Translate(0, -0.10f, 0);
+                        break;
+                    case "Level":
+                        Debug.Log("Level!");
+                        break;
+                    case "Score":
+                        break;
+                    case "Quit":
+                        Application.Quit();
+                        break;
+                    default:
+                        break;
+                }
+                    
+               
+                //Debug.Log("Mouse Down hit: " + hit.collider.name);
+
+            }
+        }
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        //disable the desired script here
+        yield return new WaitForSeconds(2F);
+        SceneManager.LoadScene("GameScene");
+        //enable it here
+    }
+}
---
+++ Assets/Scripts/onHooverMenuText.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onHooverMenuText : MonoBehaviour {
+
+    Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+    }
+
+    void  OnMouseOver()
+    {
+        
+        rend.material.color = Color.red;
+
+    }
+    void  OnMouseExit()
+    {
+        rend.material.color = Color.white;
+    }
+    // Update is called once per frame
+    void Update () {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.name == "Play")
+            {
+                //Renderer rend = GameObject.Find(hit.collider.name).GetComponent<Renderer>();
+                //rend.material.color = Color.red;
+
+            }
+            //Debug.Log("Mouse Down hit: " + hit.collider.name);
+
+        }
+    }
+}
