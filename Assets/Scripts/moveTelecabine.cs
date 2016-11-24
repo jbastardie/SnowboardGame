db106d374ef0fe234eebd2014650cc5537c45e17
BASTARDIE Jérémy <BASTARDIE Jrmy>
2016-11-24 15:00:35 +0100

    cabine avec les portes, ensemble ok

---
+++ Assets/Scripts/moveTelecabine.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveTelecabine : MonoBehaviour {
+    public Vector3 xyz;
+    public float speed;
+
+    void Start()
+    {   
+        Debug.Log(transform.position.z);
+        xyz.z = 60;
+    }
+    
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, xyz, step);
+
+    }
+}
