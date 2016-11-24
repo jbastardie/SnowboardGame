66e2375de19d6c83e597b406b9b218e16ce6e9ee
BASTARDIE Jérémy <BASTARDIE Jrmy>
2016-11-24 16:15:48 +0100

    premier test sur la telecabine, on peux aller en haut de la montagne atm

--- Assets/Scripts/moveTelecabine.cs
+++ Assets/Scripts/moveTelecabine.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class moveTelecabine : MonoBehaviour {
     public Vector3 xyz;
     public float speed;
+    private bool insideCabine = false;
+    public GameObject snowboarder;
 
     void Start()
     {   
@@ -13,8 +15,24 @@ public class moveTelecabine : MonoBehaviour {
     
     void Update()
     {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, xyz, step);
+        Debug.Log("Dans la cabine: " + insideCabine);
+        if (insideCabine)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, xyz, step); 
+        }
 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        insideCabine = true;
+        snowboarder.transform.parent = transform;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        insideCabine = false;
+        snowboarder.transform.parent = null;
     }
 }
